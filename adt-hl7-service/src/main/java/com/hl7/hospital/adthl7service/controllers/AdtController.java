6b7749fe64baffc54881c1dc4a939eab8001b2da
Carlos Gamboa Encinas <carlo-x-17@hotmail.com>
2019-03-08 16:57:16 -0400

    Added base route.

--- adt-hl7-service/src/main/java/com/hl7/hospital/adthl7service/controllers/AdtController.java
+++ adt-hl7-service/src/main/java/com/hl7/hospital/adthl7service/controllers/AdtController.java
@@ -1,5 +1,9 @@
 package com.hl7.hospital.adthl7service.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,55 +13,80 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/adt")
 public class AdtController {
 
-	@RequestMapping(value = "/" , method = RequestMethod.GET)
-	public String info() {
-		return "The messages we supasdasasport are:";
+	@RequestMapping(
+			value = "/",
+			method = RequestMethod.GET)
+	public HashMap<String, Object> info() {
+		String[] supported = {"adt-01", "adt-02" , "adt-03" , "adt-04" , "adt-05" , "adt-08", "adt-11", "adt-12", "adt-13"};
+		HashMap<String,Object> supportedADTSMap = new HashMap<String, Object>();
+		supportedADTSMap.put("HL7-version" , "2.3");
+		supportedADTSMap.put("supported-adt", supported);
+		return supportedADTSMap;
 	}
 	
 	@RequestMapping(
 			value = "/a01" , 
 			method = RequestMethod.POST)
-	public String ao1(@RequestBody String payload) {
+	public String createA01(@RequestBody String payload) {
 		return "Preparing to parse a01" + payload;
 	}
-	@RequestMapping(value = "/a02" , method = RequestMethod.POST)
-	public String ao2() {
+	
+	@RequestMapping(
+			value = "/a02",
+			method = RequestMethod.POST)
+	public String createA02() {
 		return "Preparing to parse a02";
 	}
-	@RequestMapping(value = "/a03" , method = RequestMethod.POST)
-	public String ao3() {
+	
+	@RequestMapping(
+			value = "/a03",
+			method = RequestMethod.POST)
+	public String createA03() {
 		return "Preparing to parse a03";
 	}
-	@RequestMapping(value = "/a04" , method = RequestMethod.POST)
-	public String ao4() {
+	
+	@RequestMapping(
+			value = "/a04", 
+			method = RequestMethod.POST)
+	public String createA04() {
 		return "Preparing to parse a04";
 	}
-	@RequestMapping(value = "/a05" , method = RequestMethod.POST)
-	public String ao5() {
+	
+	@RequestMapping(
+			value = "/a05",
+			method = RequestMethod.POST)
+	public String createA05() {
 		return "Preparing to parse a05";
 	}
-	@RequestMapping(value = "/a06" , method = RequestMethod.POST)
-	public String ao6() {
-		return "Preparing to parse a06";
-	}
-	@RequestMapping(value = "/a07" , method = RequestMethod.POST)
-	public String ao7() {
-		return "Preparing to parse a07";
-	}
-	@RequestMapping(value = "/a08" , method = RequestMethod.POST)
-	public String ao8() {
+	
+	
+	@RequestMapping(
+			value = "/a08",
+			method = RequestMethod.POST)
+	public String createA08() {
 		return "Preparing to parse a08";
 	}
-	@RequestMapping(value = "/a10" , method = RequestMethod.POST)
-	public String a10() {
-		return "Preparing to parse a10";
-	}
-	@RequestMapping(value = "/a11" , method = RequestMethod.POST)
-	public String a11() {
+	
+	@RequestMapping(
+			value = "/a11",
+			method = RequestMethod.POST)
+	public String createA11() {
 		return "Preparing to parse a11";
 	}
-	@RequestMapping(value = "/a12" , method = RequestMethod.POST)
-	public String a12() {
+	
+	
+	@RequestMapping(
+			value = "/a12",
+			method = RequestMethod.POST)
+	public String createA12() {
 		return "Preparing to parse a12";
+	}
+	
+	
+	@RequestMapping(
+			value = "/a13",
+			method = RequestMethod.POST)
+	public String createA13() {
+		return "Preparing to parse a13";
 	}
 }
