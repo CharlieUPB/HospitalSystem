5660b6a488783b44581745629367a67a7119c4f6
choquemeneses7 <veimar.xd@hotmail.com>
2019-03-09 19:39:44 -0400

    utils:parser finished and create methods

--- adt-hl7-service/src/main/java/utils/Parse.java
+++ adt-hl7-service/src/main/java/utils/Parse.java
@@ -1,32 +1,25 @@
 package utils;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.HapiContext;
-import ca.uhn.hl7v2.model.Message;
-import ca.uhn.hl7v2.model.v22.datatype.AD;
-import ca.uhn.hl7v2.model.v22.datatype.PN;
-import ca.uhn.hl7v2.model.v22.message.ADT_A01;
-import ca.uhn.hl7v2.model.v22.message.ADT_A02;
-import ca.uhn.hl7v2.model.v22.message.ADT_A03;
-import ca.uhn.hl7v2.model.v22.segment.MSH;
-import ca.uhn.hl7v2.parser.EncodingNotSupportedException;
-import ca.uhn.hl7v2.parser.Parser;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import ca.uhn.hl7v2.app.*;
-import ca.uhn.hl7v2.llp.LLPException;
-import ca.uhn.hl7v2.protocol.ReceivingApplication;
-import ca.uhn.hl7v2.protocol.ReceivingApplicationExceptionHandler;
+  import ca.uhn.hl7v2.model.GenericMessage;
+  import ca.uhn.hl7v2.model.Message;
+  import ca.uhn.hl7v2.parser.CanonicalModelClassFactory;
+  import ca.uhn.hl7v2.parser.DefaultModelClassFactory;
+  import ca.uhn.hl7v2.parser.GenericModelClassFactory;
+  import ca.uhn.hl7v2.parser.PipeParser;
+  import ca.uhn.hl7v2.util.Terser;
 
 
 public class Parse {
 	
 	
-	public String typeOfMessage(String message) {
+	public String typeOfMessage(String messagep) throws HL7Exception {
 		String msg = "MSH|^~\\&|HIS|RIH|EKG|EKG|199904140038||ADT^A01||P|2.2\r"
                 + "PID|0001|00009874|00001122|A00977|SMITH^JOHN^M|MOM|19581119|F|NOTREAL^LINDA^M|C|564 SPRING ST^^NEEDHAM^MA^02494^US|0002|(818)565-1551|(425)828-3344|E|S|C|0000444444|252-00-4414||||SA|||SA||||NONE|V1|0001|I|D.ER^50A^M110^01|ER|P00055|11B^M011^02|070615^BATMAN^GEORGE^L|555888^NOTREAL^BOB^K^DR^MD|777889^NOTREAL^SAM^T^DR^MD^PHD|ER|D.WT^1A^M010^01|||ER|AMB|02|070615^NOTREAL^BILL^L|ER|000001916994|D||||||||||||||||GDD|WA|NORM|02|O|02|E.IN^02D^M090^01|E.IN^01D^M080^01|199904072124|199904101200|199904101200||||5555112333|||666097^NOTREAL^MANNY^P\r"
                 + "NK1|0222555|NOTREAL^JAMES^R|FA|STREET^OTHER STREET^CITY^ST^55566|(222)111-3333|(888)999-0000|||||||ORGANIZATION\r"
@@ -36,114 +29,232 @@ public class Parse {
                 + "GT1||0222PL|NOTREAL^BOB^B||STREET^OTHER STREET^CITY^ST^77787|(444)999-3333|(222)777-5555||||MO|111-33-5555||||NOTREAL GILL N|STREET^OTHER STREET^CITY^ST^99999|(111)222-3333\r"
                 + "IN1||022254P|4558PD|BLUE CROSS|STREET^OTHER STREET^CITY^ST^00990||(333)333-6666||221K|LENIX|||19980515|19990515|||PATIENT01 TEST D||||||||||||||||||02LL|022LP554";
         HapiContext context = new DefaultHapiContext();
-        Parser p = context.getGenericParser();
+        context.setModelClassFactory(new GenericModelClassFactory());
+        GenericMessage message = (GenericMessage) context.getPipeParser().parse(messagep);
+        Terser t = new Terser(message);
+		return t.get("/MSH-8-1")+t.get("/MSH-8-2");
+		
+	}
+	
+	public Map<String, Object> ADT(String msg) throws HL7Exception {
+		
+		HapiContext context = new DefaultHapiContext();
+        context.setModelClassFactory(new GenericModelClassFactory());
 
-        Message hapiMsg;
-        try {
-            hapiMsg = p.parse(msg);
-        } catch (EncodingNotSupportedException e) {
-            e.printStackTrace();
-            return null;
-        } catch (HL7Exception e) {
-            e.printStackTrace();
-            return null;
+        String v25message = "MSH|^~\\&|system1|W|system2|UHN|200105231927||ADT^A01^ADT_A01|22139243|P|2.4\r\n" + 
+        		"EVN|A01|200105231927|\r\n" + 
+        		"PID|13|9999999999^^|2216506^||Duck^Donald^^^MR.^MR.||19720227|M|||123 Foo ST.^^TORONTO^ON^M6G 3E6^CA^H^~123 Foo ST.^^TORONTO^ON^M6G 3E6^CA^M^|1811|(416)111-1111||E^ ENGLISH|S| PATIENT DID NOT INDICATE|211004554^||||||||||||\r\n" + 
+        		"PV1|123||ZFAST TRACK^WAITING^13|E^EMERGENCY||369^6^13^U EM EMERGENCY DEPARTMENT^ZFAST TRACK WAITING^FT WAIT 13^FTWAIT13^FT WAITING^FTWAIT13|^MOUSE^MICKEY^M^^DR.^MD|||SUR||||||||I|211004554^||||||||||||||||||||W|||||200105231927|||||\r\n" + 
+        		"PV2||F|^R/O APPENDICIAL ABSCESS|||||||||||||||||||||||||\r\n" + 
+        		"IN1|1||001001|  OHIP||||||||||||^^^^^|||^^^^^^M^|||||||||||||||||||||||||^^^^^^M^|||||\r\n" + 
+        		"ACC|";
+
+        // The parser will always parse this as a "GenericMessage"
+       GenericMessage message = (GenericMessage) context.getPipeParser().parse(msg);
+        
+        /* 
+         * A generic message has a flat structure, so you can ask for any
+         * field by only its segment name, not a complex path 
+         */
+        Terser t = new Terser(message);
+        String name = t.get("/PID-5-2");
+        String lastName = t.get("/PID-5-1");
+        int codPatient = Integer.parseInt(t.get("/PID-1"));
+        int gender = 3;
+        if (t.get("/PID-8").equals("F")) {
+       	 gender = 0;
         }
-		return hapiMsg.getName();
-		
-	}
-	
-	public Map<String, String> ADT01(Message msg) {
-		
-		ADT_A01 adtMsg = (ADT_A01)msg;
-        MSH msh = adtMsg.getMSH();
-       PN patientName = adtMsg.getPID().getPatientName();
-       AD[] patientDir = adtMsg.getPID().getPatientAddress();
-       String familyName = patientName.getFamilyName().getValue();
-       String Apellido = patientName.getGivenName().getValue();
-       System.out.println(familyName);
-       System.out.println(Apellido);
-       String Direccion = null;
-       String Zip = null;
-       String City = null;
-       for (int i = 0; i < patientDir.length; i++) {
-     	  Direccion = patientDir[i].getStreetAddress().getValue();
-     	  Zip = patientDir[i].getZipOrPostalCode().getValue();
-     	  City = patientDir[i].getCity().getValue();
-			System.out.println(Direccion+" "+Zip+" "+City);
-		}
-		
-		
-	    HashMap<String, String> map = new HashMap<>();
-	    HashMap<String, String> mapdir = new HashMap<>();
-	    map.put("nombre", Apellido);
-	    map.put("apellido", familyName);
-	    map.put("direccion", Direccion);
-	    map.put("codigopostal", Zip);
-	    map.put("estado", City);
+        
+        else if (t.get("/PID-8").equals("M")) {
+       	 gender = 1;
+        }
+        else if (t.get("/PID-8").equals("O")) {
+       	 gender = 2;
+        }
+        else if (t.get("/PID-8").equals("A")) {
+       	 gender = 4;
+        }
+        else if (t.get("/PID-8").equals("N")) {
+       	 gender = 5;
+        }
+        
+        
+        String birthDate = t.get("/PID-7");
+        String phone = t.get("/PID-13");
+        String cellPhone = t.get("/PID-14");
+        String address = t.get("/PID-11-1");
+        Boolean deceased = false;
+        if (t.get("/PID-13").equals("Y")) {
+       	 deceased = true;
+        }
+        String maritalStatus = t.get("/PID-16");
+        String nationality = t.get("/PID-28");
+        String city = t.get("/PID-26");
+        
+        int codSecure = 0;
+        String nameOrganization = null;
+        String vecDate = null;
+        try {
+        codSecure = Integer.parseInt(t.get("/IN1-1"));
+        nameOrganization = t.get("/IN1-4");
+        vecDate = t.get("/IN1-13");
+        } catch (Exception e) {
+     		System.out.println("El mensaje no tiene un componente IN-1");
+     	}
+        int codDoctor = 0;
+        String nameDoctor = null;
+        String speciality = null;
+        try {
+        codDoctor = Integer.parseInt(t.get("/PV1-1"));
+        nameDoctor = t.get("/PV1-7-3");
+        speciality = t.get("/PV1-7-7");
+        } catch (Exception e) {
+    		System.out.println("El mensaje no tiene un componente PV1");
+    	}
+      
+        String diagnostic = null;
+        try {
+       	 diagnostic=t.get("/OBX-5");
+       	} catch (Exception e) {
+       		System.out.println("El mensaje no tiene un componente OBX");
+       	}
+        
+
+ 		
+ 	    HashMap<String, Object> map = new HashMap<>();
+ 	    HashMap<String, String> mapdir = new HashMap<>();
+ 	    map.put("codPatient",codPatient);
+ 	    map.put("name", name);
+ 	    map.put("lastName", lastName);
+ 	    map.put("gender", gender);
+ 	    map.put("birthDate", birthDate);
+ 	    map.put("phone", phone);
+ 	    map.put("cellPhone", cellPhone);
+ 	   map.put("address", address);
+ 	    map.put("deceased", deceased);
+ 	    map.put("maritalStatus", maritalStatus);
+ 	    map.put("nationality", nationality);
+ 	    map.put("city", city);
+ 	    map.put("codSecure", codSecure);
+ 	    map.put("nameOrganization", nameOrganization);
+ 	    map.put("vecDate", vecDate);
+ 	    map.put("codDoctor", codDoctor);
+ 	    map.put("nameDoctor", nameDoctor);
+ 	    map.put("speciality", speciality);
+ 	    map.put("diagnostic", diagnostic);
+ 	    System.out.println(map.toString());
 	    return map;
 	}	
 	
-public Map<String, String> ADT02(Message msg) {
+public Map<String, Object> ORU(String msg) throws HL7Exception {
 		
-		ADT_A02 adtMsg = (ADT_A02)msg;
-        MSH msh = adtMsg.getMSH();
-       PN patientName = adtMsg.getPID().getPatientName();
-       AD[] patientDir = adtMsg.getPID().getPatientAddress();
-       String familyName = patientName.getFamilyName().getValue();
-       String Apellido = patientName.getGivenName().getValue();
-       System.out.println(familyName);
-       System.out.println(Apellido);
-       String Direccion = null;
-       String Zip = null;
-       String City = null;
-       for (int i = 0; i < patientDir.length; i++) {
-     	  Direccion = patientDir[i].getStreetAddress().getValue();
-     	  Zip = patientDir[i].getZipOrPostalCode().getValue();
-     	  City = patientDir[i].getCity().getValue();
-			System.out.println(Direccion+" "+Zip+" "+City);
-		}
-		
-		
-	    HashMap<String, String> map = new HashMap<>();
-	    HashMap<String, String> mapdir = new HashMap<>();
-	    map.put("nombre", Apellido);
-	    map.put("apellido", familyName);
-	    map.put("direccion", Direccion);
-	    map.put("codigopostal", Zip);
-	    map.put("estado", City);
+		HapiContext context = new DefaultHapiContext();
+        context.setModelClassFactory(new GenericModelClassFactory());
+
+        String v25message = "MSH|^~\\&|system1|W|system2|UHN|200105231927||ADT^A01^ADT_A01|22139243|P|2.4\r\n" + 
+        		"EVN|A01|200105231927|\r\n" + 
+        		"PID|13|9999999999^^|2216506^||Duck^Donald^^^MR.^MR.||19720227|M|||123 Foo ST.^^TORONTO^ON^M6G 3E6^CA^H^~123 Foo ST.^^TORONTO^ON^M6G 3E6^CA^M^|1811|(416)111-1111||E^ ENGLISH|S| PATIENT DID NOT INDICATE|211004554^||||||||||||\r\n" + 
+        		"PV1|123||ZFAST TRACK^WAITING^13|E^EMERGENCY||369^6^13^U EM EMERGENCY DEPARTMENT^ZFAST TRACK WAITING^FT WAIT 13^FTWAIT13^FT WAITING^FTWAIT13|^MOUSE^MICKEY^M^^DR.^MD|||SUR||||||||I|211004554^||||||||||||||||||||W|||||200105231927|||||\r\n" + 
+        		"PV2||F|^R/O APPENDICIAL ABSCESS|||||||||||||||||||||||||\r\n" + 
+        		"IN1|1||001001|  OHIP||||||||||||^^^^^|||^^^^^^M^|||||||||||||||||||||||||^^^^^^M^|||||\r\n" + 
+        		"ACC|";
+
+        // The parser will always parse this as a "GenericMessage"
+       GenericMessage message = (GenericMessage) context.getPipeParser().parse(msg);
+        
+        /* 
+         * A generic message has a flat structure, so you can ask for any
+         * field by only its segment name, not a complex path 
+         */
+        Terser t = new Terser(message);
+        String name = t.get("/PID-5-2");
+        String lastName = t.get("/PID-5-1");
+        int codPatient = Integer.parseInt(t.get("/PID-1"));
+        int gender = 3;
+        if (t.get("/PID-8").equals("F")) {
+       	 gender = 0;
+        }
+        
+        else if (t.get("/PID-8").equals("M")) {
+       	 gender = 1;
+        }
+        else if (t.get("/PID-8").equals("O")) {
+       	 gender = 2;
+        }
+        else if (t.get("/PID-8").equals("A")) {
+       	 gender = 4;
+        }
+        else if (t.get("/PID-8").equals("N")) {
+       	 gender = 5;
+        }
+        
+        
+        String birthDate = t.get("/PID-7");
+        String phone = t.get("/PID-13");
+        String cellPhone = t.get("/PID-14");
+        String address = t.get("/PID-11-1");
+        Boolean deceased = false;
+        if (t.get("/PID-13").equals("Y")) {
+       	 deceased = true;
+        }
+        String maritalStatus = t.get("/PID-16");
+        String nationality = t.get("/PID-28");
+        String city = t.get("/PID-26");
+        
+        int codSecure = 0;
+        String nameOrganization = null;
+        String vecDate = null;
+        try {
+        codSecure = Integer.parseInt(t.get("/IN1-1"));
+        nameOrganization = t.get("/IN1-4");
+        vecDate = t.get("/IN1-13");
+        } catch (Exception e) {
+     		System.out.println("El mensaje no tiene un componente IN-1");
+     	}
+        int codDoctor = 0;
+        String nameDoctor = null;
+        String speciality = null;
+        try {
+        codDoctor = Integer.parseInt(t.get("/PV1-1"));
+        nameDoctor = t.get("/PV1-7-3");
+        speciality = t.get("/PV1-7-7");
+        } catch (Exception e) {
+    		System.out.println("El mensaje no tiene un componente PV1");
+    	}
+      
+        String diagnostic = null;
+        try {
+       	 diagnostic=t.get("/OBX-5");
+       	} catch (Exception e) {
+       		System.out.println("El mensaje no tiene un componente OBX");
+       	}
+        
+
+ 		
+ 	    HashMap<String, Object> map = new HashMap<>();
+ 	    HashMap<String, String> mapdir = new HashMap<>();
+ 	    map.put("codPatient",codPatient);
+ 	    map.put("name", name);
+ 	    map.put("lastName", lastName);
+ 	    map.put("gender", gender);
+ 	    map.put("birthDate", birthDate);
+ 	    map.put("phone", phone);
+ 	    map.put("cellPhone", cellPhone);
+ 	   map.put("address", address);
+ 	    map.put("deceased", deceased);
+ 	    map.put("maritalStatus", maritalStatus);
+ 	    map.put("nationality", nationality);
+ 	    map.put("city", city);
+ 	    map.put("codSecure", codSecure);
+ 	    map.put("nameOrganization", nameOrganization);
+ 	    map.put("vecDate", vecDate);
+ 	    map.put("codDoctor", codDoctor);
+ 	    map.put("nameDoctor", nameDoctor);
+ 	    map.put("speciality", speciality);
+ 	    map.put("diagnostic", diagnostic);
+ 	    System.out.println(map.toString());
 	    return map;
 	}	
-
-
-public Map<String, String> ADT03(Message msg) {
 	
-	ADT_A03 adtMsg = (ADT_A03)msg;
-    MSH msh = adtMsg.getMSH();
-   PN patientName = adtMsg.getPID().getPatientName();
-   AD[] patientDir = adtMsg.getPID().getPatientAddress();
-   String familyName = patientName.getFamilyName().getValue();
-   String Apellido = patientName.getGivenName().getValue();
-   System.out.println(familyName);
-   System.out.println(Apellido);
-   String Direccion = null;
-   String Zip = null;
-   String City = null;
-   for (int i = 0; i < patientDir.length; i++) {
- 	  Direccion = patientDir[i].getStreetAddress().getValue();
- 	  Zip = patientDir[i].getZipOrPostalCode().getValue();
- 	  City = patientDir[i].getCity().getValue();
-		System.out.println(Direccion+" "+Zip+" "+City);
-	}
-	
-	
-    HashMap<String, String> map = new HashMap<>();
-    HashMap<String, String> mapdir = new HashMap<>();
-    map.put("nombre", Apellido);
-    map.put("apellido", familyName);
-    map.put("direccion", Direccion);
-    map.put("codigopostal", Zip);
-    map.put("estado", City);
-    return map;
-}
 
 }
